00eda4d227ef26fcb53007be5031a2319fca44ad
eitanaizek <eitan.aizek@techork.com>
2022-01-21 21:57:50 +0600

    authorization filter

--- src/main/java/com/ruanjian/springjwt/security/SecurityConfig.java
+++ src/main/java/com/ruanjian/springjwt/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ruanjian.springjwt.security;
 
 import com.ruanjian.springjwt.filter.CustomAuthenticationFilter;
+import com.ruanjian.springjwt.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save/**").hasAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
